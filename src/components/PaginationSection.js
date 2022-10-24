049f8bef5e8817b67a50f79d5a56679d928e61a4
Adam Shtein‬‎ <78662326+SHIDRO@users.noreply.github.com>
2022-10-24 11:23:16 +0300

    Create Pagination Section

---
+++ src/components/PaginationSection.js
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { Pagination } from "@mui/material";
+
+const PaginationSection = () => {
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const onChange = (e, numOfPage) => {
+    setPageNumber(numOfPage);
+  };
+
+  useEffect(() => {
+    let responseStatus;
+
+    console.log("Fetched page number:", pageNumber);
+    // fetch(`http://localhost:8080/joker/jokes?page=${pageNumber}`)
+    //   .then((res) => {
+    //     responseStatus = res.status;
+    //     return res.json();
+    //   })
+    //   .then((resData) => {
+    //     if (responseStatus !== 201 && responseStatus !== 200) {
+    //       throw new Error(resData.message);
+    //     }
+    //     //change jokes with redux
+    //   })
+    //   .catch(console.log);
+  }, [pageNumber]);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        margin: "20px auto"
+      }}
+    >
+      {/* put dynamic number of pages */}
+      <Pagination onChange={onChange} count={10} />
+    </div>
+  );
+};
+
+export default PaginationSection;
