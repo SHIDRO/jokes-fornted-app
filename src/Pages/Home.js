c66840f2748b2730439e3dc93afce7494e96e2e2
Adam Shtein‬‎ <78662326+SHIDRO@users.noreply.github.com>
2022-10-31 11:46:06 +0200

    Update Home Page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { Box, FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+import { Box, FormControl, InputLabel, Select, MenuItem, Card, Button, CardContent, Typography } from "@mui/material";
 import Jokes from "../components/Jokes";
 import PaginationSection from "../components/PaginationSection";
 import { useSelector } from "react-redux";
 import SearchBar from "../components/SearchBar";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const [chosenHashtag, setChosenHashtag] = useState("");
@@ -11,22 +12,34 @@ const Home = () => {
   const { globalJokes, hashtags } = useSelector((state) => state.jokesData);
   const [searchInput, setSearchInput] = useState(null);
   let homeJokes;
+  let foundHashtags = [];
 
-  if(!searchInput){
+  const navigate = useNavigate()
+
+  if (!searchInput) {
     homeJokes = globalJokes
-  }else {
-    homeJokes = globalJokes.filter(j => {
-       if(j.content.includes(searchInput)){
-        return true
-       }
+  } else {
+    foundHashtags = hashtags.filter(h => h.hashtag.includes(searchInput));
+    if (foundHashtags.length === 0) {
+      const filter = searchInput.substring(0, searchInput.length - 2);
+      console.log("filter", filter)
+      foundHashtags = hashtags.filter(h => h.hashtag.includes(filter));
+    }
 
-       if(j.title.includes(searchInput)) return true;
+    homeJokes = globalJokes.filter(j => {
+      if (j.content.includes(searchInput)) {
+        return true
+      }
+
+      if (j.title.includes(searchInput)) return true;
     })
   }
 
-  console.log('Home Jokes',homeJokes)
+  console.log('Home Jokes', homeJokes)
+  console.log('Hashtags', foundHashtags)
 
-  if(chosenHashtag !== ""){
+
+  if (chosenHashtag !== "") {
     homeJokes = globalJokes.filter(j => {
       return j.hashtag.hashtag === chosenHashtag
     })
@@ -39,12 +52,16 @@ const Home = () => {
     setChosenHashtagColor(color);
   };
   console.log(chosenHashtagColor);
+
+  const navigateHashtag = ({hashtag}) => {
+    navigate(`/${hashtag}`)
+  }
   return (
     <div>
       <h1>Home</h1>
-      <SearchBar setSearchInput={setSearchInput}/>
+      <SearchBar setSearchInput={setSearchInput} />
       <Box sx={{ maxWidth: 180, float: "right" }}>
-        <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
+        {/* <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
           <InputLabel id="demo-simple-select-filled-label">#</InputLabel>
           <Select
             sx={{ color: chosenHashtagColor, fontWeight: "bold" }}
@@ -57,7 +74,21 @@ const Home = () => {
               return <MenuItem key={h._id} value={h.hashtag}>{h.hashtag}</MenuItem>;
             })}
           </Select>
-        </FormControl>
+        </FormControl> */}
+        <div style={{ border: '1px solid black', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignContent: 'center', margin: 'auto' }}>
+          {foundHashtags.map(hashtag => {
+            return (
+              <div>
+                <Card sx={{ padding: '0px' }} key={hashtag._id} elevation={6}>
+                  <CardContent sx={{ padding: '0px' }}>
+                    <Button onClick={navigateHashtag.bind(this, hashtag)} variant="text" sx={{ color: hashtag.color, cursor: 'pointer', padding: '0' }}>#{hashtag.hashtag}</Button>
+                  </CardContent>
+                </Card>
+                <hr />
+              </div>
+            )
+          })}
+        </div>
       </Box>
       <Jokes jokes={homeJokes} />
       <PaginationSection numOfPages={100} />
